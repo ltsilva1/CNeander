eecec6504a45520ef800211014cd4608fe3d8fe8
lucas <lucastelles2005@outlook.com>
2025-07-20 22:39:09 -0300

    Alterações na impressão de memória

--- include/print_functions.h
+++ include/print_functions.h
@@ -4,10 +4,12 @@
 
 #ifndef PRINT_FUNCTIONS_H
 #define PRINT_FUNCTIONS_H
+#include <stdio.h>
+
 #include "memory.h"
 
 void printMemory(Neander cpu);
-void printMemoryVARS(Neander cpu);
+void printMemoryVARS(Neander cpu, FILE* arq);
 void printMemoryASIS(Neander cpu);
 void printStates(Neander cpu);
 void printToTxt(Neander cpu, const char nomeArquivo[]);
--- src/print_functions.c
+++ src/print_functions.c
@@ -7,9 +7,9 @@
 void printMemory(Neander cpu) {
     printf("\nInstrucoes na memoria:\n");
     int i = 0;
-    while (i < 256) {
+    while (i < 128) {
         uint8_t opcode = cpu.memoria[i];
-        if (i + 1 < 256) { // Guarda de acesso
+        if (i + 1 < 128) { // Guarda de acesso
             switch (opcode) {
                 case  16:
                     printf("STA %03d\n", cpu.memoria[i+1]);
@@ -50,7 +50,7 @@ void printMemory(Neander cpu) {
                 case  240:
                     printf("HLT\n");
                     i += 1;
-                    break;
+                    return;
                 default:
                     printf("NOP\n");
                     i += 1;
@@ -65,10 +65,10 @@ void printMemory(Neander cpu) {
     }
 }
 
-void printMemoryVARS(Neander cpu) {
+void printMemoryVARS(Neander cpu, FILE *arq) {
+    fprintf(arq,"Variaveis:\n");
     for (int i = 128; i < 256; i++)
-        printf("%03d: %02d  ", i, cpu.memoria[i]);
-    printf("\n");
+        fprintf(arq,"%03d: %02d\n", i, cpu.memoria[i]);
 }
 
 void printMemoryASIS(Neander cpu) {
@@ -88,61 +88,62 @@ void printToTxt(Neander cpu, const char nomeArquivo[]) {
 
     FILE* arquivoTXT = fopen(nomeArquivoTXT, "w");
     int i = 0;
-    while (i < 256) {
+    while (i < 128) {
         uint8_t opcode = cpu.memoria[i];
-        if (i + 1 < 256) { // Guarda de acesso
+        if (i + 1 < 128) { // Guarda de acesso
             switch (opcode) {
                 case  16:
-                    fprintf(arquivoTXT, "STA %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: STA %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  32:
-                    fprintf(arquivoTXT, "LDA %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: LDA %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  48:
-                    fprintf(arquivoTXT, "ADD %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: ADD %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  64:
-                    fprintf(arquivoTXT, "OR %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: OR %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  80:
-                    fprintf(arquivoTXT, "AND %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: AND %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  96:
-                    fprintf(arquivoTXT, "NOT\n");
+                    fprintf(arquivoTXT, "%03d: NOT\n", i);
                     i += 1;
                     break;
                 case  128:
-                    fprintf(arquivoTXT, "JMP %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: JMP %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  144:
-                    fprintf(arquivoTXT, "JN  %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: JN  %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  160:
-                    fprintf(arquivoTXT, "JZ  %03d\n", cpu.memoria[i+1]);
+                    fprintf(arquivoTXT, "%03d: JZ  %03d\n", i, cpu.memoria[i+1]);
                     i += 2;
                     break;
                 case  240:
-                    fprintf(arquivoTXT, "HLT\n");
+                    fprintf(arquivoTXT, "%03d: HLT\n", i);
                     i += 1;
                     break;
                 default:
-                    fprintf(arquivoTXT, "%03d\n", opcode);
+                    fprintf(arquivoTXT, "%03d: %03d\n", i, opcode);
                     i += 1;
                     break;
             }
         } else {
-            if (opcode == 96) fprintf(arquivoTXT, "NOT\n");
-            else if (opcode == 240) fprintf(arquivoTXT, "HLT\n");
-            else fprintf(arquivoTXT, "%03d\n", opcode);
+            if (opcode == 96) fprintf(arquivoTXT, "%03d: NOT\n", i);
+            else if (opcode == 240) fprintf(arquivoTXT, "%03d: HLT\n", i);
+            else fprintf(arquivoTXT, "%03d: %03d\n", i, opcode);
             i += 1;
         }
     }
+    printMemoryVARS(cpu, arquivoTXT);
     printf("Arquivo de texto exportado!\n");
 }
